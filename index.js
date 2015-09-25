f6497afbc30ddda1685e8ffb8a251d00b72638c8
nswbmw <gxqzk@126.com>
2015-09-25 17:02:43 +0800

    bump v1.1.0

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 'use strict';
 
-let merge = require('merge-descriptors');
-let slowlog = require('co-slowlog')();
 let pkg = require('pkg-up').sync();
 
 module.exports = function (options) {
@@ -9,15 +7,17 @@ module.exports = function (options) {
   options.name = options.name || (require(pkg) || {}).name;
   options.input = options.output = options.fn = options.filename = undefined;
 
+  let slowlog = require('co-slowlog')(options);
+
   return function* (next) {
     yield slowlog(function* () {
       yield next;
-    }, merge({
+    }, {
       method: this.method,
       url: this.url,
       ips: this.ips,
       headers: this.request.headers,
       body: this.request.body
-    }, options));
+    });
   };
 };
